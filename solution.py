cddb3eedf3f6454381bf8e9f02f21b6ac0112b02
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-11 09:06:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 chat_id = 157443210 # Ваш chat ID, не меняйте название переменной
 
@@ -8,6 +8,8 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    p_value = anderson_ksamp([x, y]).pvalue 
+    p_value = MMD(compute_kernel="rbf", gamma=10).test(x, y).pvalue    
     alpha = 0.08
     return p_value < alpha # Ваш ответ, True или False
+
+
