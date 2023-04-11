6940e1718c92c1f5331da1053f446221d4bdee49
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-11 09:27:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import MMD
+import scipy.stats as sps
 
 chat_id = 157443210 # Ваш chat ID, не меняйте название переменной
 
@@ -8,7 +8,7 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    p_value = MMD(compute_kernel="rbf", gamma=10).test(x, y).pvalue    
+    p_value = sps.cramervonmises_2samp(x, y).pvalue    
     alpha = 0.08
     return p_value < alpha # Ваш ответ, True или False
 
