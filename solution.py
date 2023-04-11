3bfb997727adeb483e6dbcfa8d1decb6ba0b0e1e
artdem89 <82702367+artdem89@users.noreply.github.com>
2023-04-11 07:46:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,5 +9,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
      p_value = anderson_ksamp([x, y]).pvalue 
-    alpha = 0.07
+    alpha = 0.08
     return p_value < alpha # Ваш ответ, True или False
